0677fc49101fed42ff3abb2da041682ca2de6223
kisandris1 <115239644+kisandris1@users.noreply.github.com>
2023-12-12 11:03:00 +0000

    test before share

--- tester.py
+++ tester.py
@@ -4,8 +4,11 @@ import pandas as pd
 
 start = time.perf_counter()
 
-data = RequestData.DiabAPIreq(username="test", collection="andersondataset")
-
+data = RequestData.DiabAPIreq(username="sotetestkey", collection="andersondataset")
+print(len(data))
 endreq = time.perf_counter()
 ms1 = (endreq - start)
 print("done " + str(ms1))
+
+#1533127
+#done 64.43663508400005
